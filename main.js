b93eed63788b7de7049a2e4e2dfaabae588bc83b
Morshed-Ahmed <morshed50r@gmail.com>
2021-08-21 02:13:49 +0600

    My first action is ready

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-console.log('hellow');
+
+
+document.getElementById('memory-8gb').addEventListener('click', function () {
+    const memoryCost = document.getElementById('memory-cost');
+    const costPrice = memoryCost.innerText;
+    const cos = parseInt(costPrice);
+    //console.log(typeof cos);
+})
+document.getElementById('memory-16gb').addEventListener('click', function () {
+    const memoryCost = document.getElementById('memory-cost');
+    const costPrice = memoryCost.innerText;
+    const cos = parseInt(costPrice);
+    console.log('hellow');
+})
